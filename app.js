28bb8b69e5d513afe661f6803bb6a516d0c3468a
Josh Peterson <joshuadanpeterson@gmail.com>
2024-01-12 20:23:20 -0700

    Passport

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 // app.js
 
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+// var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+// Set up express-session
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+// Import passport for authentication
+const passport = require('passport');
+const authenticate = require('./authenticate');
+
+// Setup Passport strategies
+// authenticate.setup(passport);
+
 // Import routers
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 
-// Set up express-session
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
-
 // Create Mongoose server
 const mongoose = require('mongoose');
+
 const url = 'mongodb://127.0.0.1:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
@@ -23,27 +37,7 @@ connect.then(() => console.log('Connected correctly to server'),
     err => console.log(err)
 );
 
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-// var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
 var app = express()
-
-// Request test
-app.use((req, res, next) => {
-	console.log('Incoming request:', req.method, req.url);
-	next();
-});
-
-// Test
-app.get("/test", (req, res) => {
-	res.send("Server is working");
-});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -52,8 +46,7 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-//app.use(sessionParser('12345-67890-09876-54321'));
+//app.use(cookieParser('12345-67890-09876-54321'));
 
 app.use(session({
     name: 'session-id',
@@ -63,26 +56,25 @@ app.use(session({
     store: new FileStore()
 }));
 
+// Import passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Route Definitions
 app.use("/", indexRouter);
+// Routes that don't require authentication
 app.use("/users", usersRouter);
 
 // Set up user authentication support
 function auth(req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-    if (!req.session.user) {
-        const err = new Error('You are not authenticated!');
+    if (!req.user) {
+        const err = new Error('You are not authenticated!');                    
         err.status = 401;
         return next(err);
     } else {
-        if (req.session.user === 'authenticated') {
-            return next();
-        } else {
-            const err = new Error('You are not authenticated!');
-            err.status = 401;
-            return next(err);
-        }
+        return next();
     }
 }
 
@@ -100,7 +92,7 @@ app.get("/campsites/test", (req, res) => {
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+    next(createError(404));
 });
 
 // error handler
@@ -116,5 +108,6 @@ app.use(function (err, req, res, next) {
 
 // Serve Static Files Before Authentication
 app.use(express.static(path.join(__dirname, "public")));
+
 
 module.exports = app;
