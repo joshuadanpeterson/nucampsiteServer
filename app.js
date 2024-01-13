9cce007146afa1146d70f998658837847cbdbd10
Josh Peterson <joshuadanpeterson@gmail.com>
2024-01-12 18:13:33 -0700

    Express Session Part 2

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
+// app.js
+
+// Import routers
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
 
-const mongoose = require('mongoose');
+// Set up express-session
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
+// Create Mongoose server
+const mongoose = require('mongoose');
 const url = 'mongodb://127.0.0.1:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
@@ -19,7 +26,7 @@ connect.then(() => console.log('Connected correctly to server'),
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
+// var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
@@ -42,37 +49,34 @@ app.get("/test", (req, res) => {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser('12345-67890-09876-54321'));
+//app.use(sessionParser('12345-67890-09876-54321'));
 
+app.use(session({
+    name: 'session-id',
+    secret: '12345-67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+}));
+
+// Route Definitions
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// Set up user authentication support
 function auth(req, res, next) {
-    if (!req.signedCookies.user) {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            const err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-  
-        const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        const user = auth[0];
-        const pass = auth[1];
-        if (user === 'admin' && pass === 'password') {
-            res.cookie('user', 'admin', {signed: true});
-            return next(); // authorized
-        } else {
-            const err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
+    console.log(req.session);
+
+    if (!req.session.user) {
+        const err = new Error('You are not authenticated!');
+        err.status = 401;
+        return next(err);
     } else {
-        if (req.signedCookies.user === 'admin') {
+        if (req.session.user === 'authenticated') {
             return next();
         } else {
             const err = new Error('You are not authenticated!');
@@ -84,22 +88,15 @@ function auth(req, res, next) {
 
 app.use(auth);
 
-// Route Definitions
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
 // Set up Nucamp routers
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
 
-// Static file serving
-app.use(express.static(path.join(__dirname, "public")));
-
 // Set up Campsites router test
 app.get("/campsites/test", (req, res) => {
   res.send("Campsites route is working");
 });
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -116,5 +113,8 @@ app.use(function (err, req, res, next) {
 	res.status(err.status || 500);
 	res.render("error");
 });
+
+// Serve Static Files Before Authentication
+app.use(express.static(path.join(__dirname, "public")));
 
 module.exports = app;
