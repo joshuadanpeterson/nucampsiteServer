466abc1b4a02e54ae8e67ac7b5e78094d85da95e
Josh Peterson <joshuadanpeterson@gmail.com>
2024-01-13 15:46:43 -0700

    Nucamp NodeJS Week 3 Workshop

--- authenticate.js
+++ authenticate.js
@@ -1,20 +1,34 @@
 // authenticate.js
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const User = require('./models/user');
-
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
-
-const config = require('./config.js');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("./models/user");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
+const config = require("./config.js");
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = function(user) {
-    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+exports.getToken = function (user) {
+	return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
+};
+
+// Middleware for user authentication
+exports.verifyUser = passport.authenticate("jwt", { session: false });
+
+// Place the verifyAdmin function here, after verifyUser
+exports.verifyAdmin = (req, res, next) => {
+	if (req.user && req.user.admin) {
+		next(); // User is an admin
+	} else {
+		const err = new Error(
+			"You are not authorized to perform this operation!"
+		);
+		err.status = 403; // Forbidden status code
+		next(err);
+	}
 };
 
 const opts = {};
@@ -22,21 +36,16 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(
-    new JwtStrategy(
-        opts,
-        (jwt_payload, done) => {
-            console.log('JWT payload:', jwt_payload);
-            User.findOne({_id: jwt_payload._id}, (err, user) => {
-                if (err) {
-                    return done(err, false);
-                } else if (user) {
-                    return done(null, user);
-                } else {
-                    return done(null, false);
-                }
-            });
-        }
-    )
+	new JwtStrategy(opts, (jwt_payload, done) => {
+		console.log("JWT payload:", jwt_payload);
+		User.findOne({ _id: jwt_payload._id }, (err, user) => {
+			if (err) {
+				return done(err, false);
+			} else if (user) {
+				return done(null, user);
+			} else {
+				return done(null, false);
+			}
+		});
+	})
 );
-
-exports.verifyUser = passport.authenticate('jwt', {session: false});
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,103 +1,122 @@
 const express = require("express");
 const Promotion = require("../models/promotion");
 const promotionRouter = express.Router();
-const authenticate = require('../authenticate');
+const authenticate = require("../authenticate");
 
-promotionRouter.route("/")
-  .get((req, res, next) => {
-    Promotion.find()
-      .then(promotions => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(promotions);
-      })
-      .catch(err => next(err));
-  })
- .post(authenticate.verifyUser, (req, res, next) => {
-    console.log("POST request body:", req.body);
-    console.log("Attempting to create promotion...");
+promotionRouter
+	.route("/")
+	.get((req, res, next) => {
+		Promotion.find()
+			.then((promotions) => {
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "application/json");
+				res.json(promotions);
+			})
+			.catch((err) => next(err));
+	})
+	.post(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			console.log("POST request body:", req.body);
+			console.log("Attempting to create promotion...");
 
-  Promotion.create(req.body)
-    .then(promotion => {
-      console.log("Promotion Created", promotion);
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json(promotion);
-    })
-    .catch(err => {
-      console.error("Error during Promotion creation:", err);
-      console.error(err);
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({
-        error: err.message,
-        fullError: err
-      });
-    });
-})
-.put(authenticate.verifyUser, (req, res) => {
-  res.statusCode = 403;
-  res.end("PUT operation not supported on /promotions");
-})
-.delete(authenticate.verifyUser, (req, res, next) => {
-  Promotion.deleteMany()
-    .then((response) => {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.json(response);
-    })
-    .catch((err) => {
-      console.error("Error:", err); // Log the error
-      next(err);
-    });
-});
+			Promotion.create(req.body)
+				.then((promotion) => {
+					console.log("Promotion Created", promotion);
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(promotion);
+				})
+				.catch((err) => {
+					console.error("Error during Promotion creation:", err);
+					console.error(err);
+					res.statusCode = 500;
+					res.setHeader("Content-Type", "application/json");
+					res.json({
+						error: err.message,
+						fullError: err,
+					});
+				});
+		}
+	)
+	.put(authenticate.verifyUser, (req, res) => {
+		res.statusCode = 403;
+		res.end("PUT operation not supported on /promotions");
+	})
+	.delete(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Promotion.deleteMany()
+				.then((response) => {
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(response);
+				})
+				.catch((err) => {
+					console.error("Error:", err); // Log the error
+					next(err);
+				});
+		}
+	);
 
-promotionRouter.route("/:promotionId")
-  .get((req, res, next) => {
-    console.log("Requested Promotion ID:", req.params.promotionId);
-    Promotion.findById(req.params.promotionId)
-      .then((promotion) => {
-        console.log("Found Promotion:",
-          promotion); // Log the found promotions
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.json(promotion);
-      })
-      .catch(err => {
-        console.error("Error retrieving promotion:", err);
-        next(err);
-      });
-  })
-.post(authenticate.verifyUser, (req, res) => {
-  res.statusCode = 403;
-  res.end(
-    `POST operation not supported on /promotions/${req.params.promotionId}`
-  );
-})
-.put(authenticate.verifyUser, (req, res, next) => {
-  Promotion.findByIdAndUpdate(
-      req.params.promotionId, {
-        $set: req.body,
-      }, {
-        new: true
-      }
-    )
-    .then((promotion) => {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.json(promotion);
-    })
-    .catch((err) => next(err));
-})
-.delete(authenticate.verifyUser, (req, res, next) => {
-  Promotion.findByIdAn.delete(authenticate.verifyUser, req.params
-      .promotionId)
-    .then((response) => {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.json(response);
-    })
-    .catch((err) => next(err));
-});
+promotionRouter
+	.route("/:promotionId")
+	.get((req, res, next) => {
+		console.log("Requested Promotion ID:", req.params.promotionId);
+		Promotion.findById(req.params.promotionId)
+			.then((promotion) => {
+				console.log("Found Promotion:", promotion); // Log the found promotions
+				res.statusCode = 200;
+				res.setHeader("Content-Type", "application/json");
+				res.json(promotion);
+			})
+			.catch((err) => {
+				console.error("Error retrieving promotion:", err);
+				next(err);
+			});
+	})
+	.post(authenticate.verifyUser, (req, res) => {
+		res.statusCode = 403;
+		res.end(
+			`POST operation not supported on /promotions/${req.params.promotionId}`
+		);
+	})
+	.put(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Promotion.findByIdAndUpdate(
+				req.params.promotionId,
+				{
+					$set: req.body,
+				},
+				{
+					new: true,
+				}
+			)
+				.then((promotion) => {
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(promotion);
+				})
+				.catch((err) => next(err));
+		}
+	)
+	.delete(
+		authenticate.verifyUser,
+		authenticate.verifyAdmin,
+		(req, res, next) => {
+			Promotion.findByIdAn
+				.delete(authenticate.verifyUser, req.params.promotionId)
+				.then((response) => {
+					res.statusCode = 200;
+					res.setHeader("Content-Type", "application/json");
+					res.json(response);
+				})
+				.catch((err) => next(err));
+		}
+	);
 
 module.exports = promotionRouter;
